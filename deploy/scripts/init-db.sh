be2a53463df34d30ba9d27d9c8aef6bf4612af1e
peplxx <91543105+peplxx@users.noreply.github.com>
2025-03-17 14:53:01 +0300

    feat: add startup automigrator

--- deploy/scripts/init-db.sh
+++ deploy/scripts/init-db.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # This script runs on the database container startup
 
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -f /tmp/startup_migration.sql
